b2eed558a0ba18379e1522489c53cf9eb4e8b662
catrix <kevyn@catrixs-MacBook-Pro.local>
2023-01-03 18:04:58 +0100

    fix ex02

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -1,19 +1,28 @@
-#ifndef BASE_HPP
-# define BASE_HPP
-# include <iostream>
-# include <string>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolin <acolin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 14:01:12 by acolin            #+#    #+#             */
+/*   Updated: 2022/03/23 14:01:12 by acolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
 
 class Base
 {
 	public:
-		virtual ~Base( void );
-		Base *	generate ( void );
-		void	identify( Base * p );
-		void	identify( Base & p );
+		virtual ~Base(void) {};
 };
 
 class A: public Base {};
 class B: public Base {};
 class C: public Base {};
 
-#endif
+Base*	generate(void);
+void	identify(Base *p);
+void	identify(Base &p);
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,16 +1,69 @@
-#include "Convert.hpp"
-#include "Base.hpp"
-#include <unistd.h> 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolin <acolin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 14:00:53 by acolin            #+#    #+#             */
+/*   Updated: 2022/03/23 14:00:53 by acolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+#include "Base.hpp"
+
+Base*	generate(void)
 {
-	Base u;
-	Base *b;
-	Base *c;
-	b = u.generate();
-	usleep(500000);
-	c = u.generate();
-	u.identify(b);
-	u.identify(*c);
-	return 0 ;
+	srand(time(NULL));
+	switch (rand() % 3)
+	{
+		case 1:
+			return (new A());
+		case 2:
+			return (new B());
+		default:
+			return (new C());
+	}
 }
+
+void	identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void	identify(Base &p)
+{
+	try {
+		(void) dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception &e) {
+		try {
+			(void) dynamic_cast<B&>(p);
+			std::cout << "B" << std::endl;
+		}
+		catch (std::exception &e) {
+			try {
+				(void) dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			}
+			catch (std::exception &e) { }
+		}
+	}
+}
+
+int main(void)
+{
+	Base *base;
+	
+	base = generate();
+    identify(base);
+    identify(*base);
+	return (0);
+}
+
